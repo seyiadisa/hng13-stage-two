0271a70ec93c128eede6844ded890e06885d3fb8
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2025-10-25 11:03:49 +0100

    init apps

---
+++ twig/src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__."/templates");
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index.twig', ['name' => 'World']);
